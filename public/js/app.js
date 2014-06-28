6f425434e0bbd6df2ea6183f0ea7b242896c7e20
canuckistani <jeff@canuckistani.ca>
2014-06-27 18:22:52 -0700

    switched to a highcharts bar chart, added tolbox, now sorted in descending order.

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ function cap(str) {
 }
 
 var tools = {
-  // 'Toolbox':            'DEVTOOLS_TOOLBOX_OPENED_PER_USER_FLAG',
+  'Toolbox':            'DEVTOOLS_TOOLBOX_OPENED_PER_USER_FLAG',
   'Inspector':          'DEVTOOLS_INSPECTOR_OPENED_PER_USER_FLAG',
   'Web Console':        'DEVTOOLS_WEBCONSOLE_OPENED_PER_USER_FLAG',
   'Net Monitor':        'DEVTOOLS_NETMONITOR_OPENED_PER_USER_FLAG',
@@ -46,7 +46,7 @@ function fetchChannel(targetVersions, channel, finish) {
           if (_i === limit) {
             // sum up totals for each channels / tool combination
 
-            finish(_.sortBy(totals, "label"));
+            finish(_.sortBy(totals, "yes").reverse());
           }
         });
       });
@@ -56,14 +56,23 @@ function fetchChannel(targetVersions, channel, finish) {
 
 $(function() {
 
+  $('#reload-btn').click(function() {
+    fetch(render);
+  });
+
+  fetch(render);
+});
+
+function fetch(callback) {
+
   $('#about-toggle').click(function(e) {
     e.preventDefault();
     $('#about-container').toggle();
   });
 
   var channels = {
-    'aurora'  : [31],
-    'nightly'  : [32],
+    'aurora'  : [32],
+    'nightly'  : [33],
   };
 
   var pair_channels = _.pairs(channels);
@@ -76,43 +85,120 @@ $(function() {
   var r = [], _i = 0;
   var chart_columns = [];
 
-  _.each(channels, function(versions, channel) {
-    $('.'+channel).html([cap(channel), versions[0]].join(' '));
-    fetchChannel(versions, channel, function(data) {
 
-
-      if (chart_columns.length === 0) {
-        chart_columns = _.map(data, function(item) {
-          return item.label;
-        });
-        chart_struct.labels = chart_columns;
-      }
-
-      var _str_columns = _.keys(data[0]).join(',')+"\n";
-
-      // var _str_data = _.map(data, function(row) {
-      //   return _.values(row).join(',');
-      // }).join("\n");
-
-      // $('pre#output').append(_str_columns+_str_data+"\n");
-
-      chart_data = _.map(data, function(row) {
-        return row.yes;
+  var functions = _.map(channels, function(versions, channel) {
+    return function(callback) {
+      fetchChannel(versions, channel, function(data) {
+        callback(null, {channel: channel, data: data});
       });
-
-      var color = $('.'+channel).css('background-color');
-
-      chart_struct.datasets.push({
-        fillColor: color,
-        strokeColor: color,
-        data: chart_data
-      });
-
-      if (chart_struct.datasets.length === _.size(channels)) {
-
-        var ctx = $("#data-chart").get(0).getContext("2d");
-        var _chart = new Chart(ctx).Bar(chart_struct);
-      }
-    });
+    };
   });
-});
+
+  async.parallel(functions, function(err, results) {
+    if (err) throw err;
+    callback(results);
+  });
+
+  // _.each(channels, function(versions, channel) {
+  //   fetchChannel(versions, channel, function(data) {
+
+  //     console.log(data);
+
+  //     callback(data);
+
+  //     // if (chart_columns.length === 0) {
+  //     //   chart_columns = _.map(data, function(item) {
+  //     //     return item.label;
+  //     //   });
+  //     //   chart_struct.labels = chart_columns;
+  //     // }
+
+  //     // var _str_columns = _.keys(data[0]).join(',')+"\n";
+
+  //     // // var _str_data = _.map(data, function(row) {
+  //     // //   return _.values(row).join(',');
+  //     // // }).join("\n");
+
+  //     // // $('pre#output').append(_str_columns+_str_data+"\n");
+
+  //     // chart_data = _.map(data, function(row) {
+  //     //   return row.yes;
+  //     // });
+
+  //     // var color = $('.'+channel).css('background-color');
+
+  //     // chart_struct.datasets.push({
+  //     //   fillColor: color,
+  //     //   strokeColor: color,
+  //     //   data: chart_data
+  //     // });
+
+  //     // if (chart_struct.datasets.length === _.size(channels)) {
+
+  //     //   var ctx = $("#data-chart").get(0).getContext("2d");
+  //     //   var _chart = new Chart(ctx).Bar(chart_struct);
+  //     // }
+  //   });
+  // });
+};
+
+function render(data) {
+  var categories = _.pluck(data[0].data, 'label');
+  var series = [];
+
+  var totals = {};
+
+
+  _.each(data, function(channel) {
+    _tool = {name: channel.channel};
+    _tool.data = _.pluck(channel.data, 'yes');
+    _.each(channel.data, function(d) {
+      if (!totals[d.label]) {
+        totals[d.label] = {count: d.yes, name: d.label};  
+      }
+      else {
+        totals[d.label].count += d.yes;
+      }
+    })
+    series.push(_tool);
+  });
+
+  // console.log(_.sortBy(totals, 'count'));
+
+  var graph = {
+      chart: {
+          type: 'column',
+          height: 600,
+      },
+      title: {
+          text: 'Tool usage'
+      },
+      xAxis: {
+          // categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+          categories: categories
+      },
+      yAxis: {
+          title: {
+              text: null
+          },
+          labels: {
+              formatter: function(){
+                  return (Math.abs(this.value) / 1000) + 'K';
+              }
+          },
+          min: 0,
+          max: 30000
+      },
+      plotOptions: {
+          series: {
+              stacking: 'normal'
+          }
+      },
+      series: series
+  };
+
+  console.log(graph);
+
+  $('#graph-container').highcharts(graph);
+
+}
