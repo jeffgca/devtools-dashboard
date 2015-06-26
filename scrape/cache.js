7aa915d88955e39ab3b8abfd0c24a006e3fe524d
canuckistani <jeff@canuckistani.ca>
2015-06-26 13:41:38 -0700

    no more phantom

--- scrape/cache.js
+++ scrape/cache.js
@@ -12,34 +12,30 @@ var dd = new DevtoolsTelemetry();
 var _ = require('underscore');
 var request = require('request');
 
-request.get('http://fxver.paas.canuckistani.ca/', function(e, r, b) {
-  if (e) throw e;
-  // console.log("erb>", e, r, b);
-  console.log("body>", JSON.parse(b));
-});
-
 function getWindows(callback) {
   request.get('http://fxver.paas.canuckistani.ca/', function(e, r, b) {
     if (e) throw e;
     var _ver = JSON.parse(b);
     // console.log("recent", parseInt(_ver.nightly));
     var windows = dd.generateBuildWindows(35, parseInt(_ver.nightly));
-    dd.getWeeklyToolUsage(windows, 'Toolbox', callback);
+    callback(null, windows);
   });
 }
 
 function main(command, callback) {
   dd.init(function() {
     switch(command) {
-      case 'weekly':
-        request.get('http://fxver.paas.canuckistani.ca/', function(e, r, b) {
-          if (e) throw e;
-          var _ver = JSON.parse(b);
-          // console.log("recent", parseInt(_ver.nightly));
-          var windows = dd.generateBuildWindows(35, parseInt(_ver.aurora));
+      case 'tool':
+        getWindows(function(err, windows) {
+          if (err) throw err;
           dd.getWeeklyToolUsage(windows, 'Toolbox', callback);
         });
         break;
+      case 'channels':
+        getWindows(function(err, windows) {
+          if (err) throw err;
+          dd.getWeeklyChannelUsage(windows, 'Toolbox', callback);
+        });
       default:
         break;
     }
